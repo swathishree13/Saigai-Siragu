1ac7ade9c68385446503ced4320899e14611450b
swathishree13 <swathishree2007@gmail.com>
2026-09-19 15:02:19 +0530

    Connect Vercel frontend to PPTX renderer

--- js/renderer-config.js
+++ js/renderer-config.js
@@ -1,2 +1,2 @@
-// Set this to the public HTTPS URL of the Docker/Python PPTX renderer.
-window.CLEARSTAGE_RENDERER_URL = '';
+// Public HTTPS URL of the Docker/Python PPTX renderer.
+window.CLEARSTAGE_RENDERER_URL = 'https://saigai-siragu.onrender.com';
